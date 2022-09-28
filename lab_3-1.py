33ec24de222d4a4364fb3ff313215d1e8486e919
Colin Sasser <p23csasser@fairfieldprep.org>
2022-09-28 10:29:23 -0400

    Submission

---
+++ lab_3-1.py
@@ -0,0 +1,9 @@
+# Creator Cs 9/27/2022
+
+magic_charge = 100
+shield_charge = 80
+
+if (magic_charge < 90) or (shield_charge < 75):
+   print ("The dragon burns you to a crisp.")
+else:
+   print ("You defeated the dragon! But the princess is in another castle.")
---
+++ lab_3-2.py
@@ -0,0 +1,12 @@
+# Creator CS 9/27/2022
+
+# int(a) # NameError
+
+#int('a') # Value Error
+
+# 'a' + 2   #Type Error
+
+# import date  #ModuleNotFoundError
+
+# print("Im a happy camper!) #SyntaxError
+
