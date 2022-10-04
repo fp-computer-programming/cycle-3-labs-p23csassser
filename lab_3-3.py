f14f0f7b82a86751468aba607b6e140a17d8848c
Colin Sasser <p23csasser@fairfieldprep.org>
2022-10-04 09:30:26 -0400

    Submission

---
+++ lab_3-3.py
@@ -0,0 +1,9 @@
+# Creator CS 9/28/2022
+
+magic_charge = 100
+shield_charge = 80
+
+if (magic_charge < 90) or (shield_charge < 75):
+   print ("The dragon burns you to a crisp.")
+else:
+   print ("Kristen Sark.")
